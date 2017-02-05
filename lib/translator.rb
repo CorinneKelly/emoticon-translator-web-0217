2c0e67e2d677aedf43431ddd9b02c73f16133876
Corinne Kelly <corinnemariekelly@gmail.com>
2017-02-05 22:08:56 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,44 @@
 # require modules here
+require "yaml"
+require 'byebug'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  emoticons = YAML.load_file(file_path)
+  final_hash = {"get_meaning" => {}, "get_emoticon" => {}}
+
+  emoticons.each do |feeling, emoticon_array|
+    # ex. {happy => [";)", "-__-"], confused => [";)", "-__-"]}
+    emoticon_array.each_with_index do |emoticon, index|
+      # [";)", "-__-"]
+      if index == 1
+        # final_hash = {"get_meaning" => { -__- => happy }}
+        final_hash["get_meaning"][emoticon] = feeling
+        # # final_hash = {"get_emoticon" => { ;) => -____- }}
+        final_hash["get_emoticon"][emoticon_array[0]] = emoticon
+      end
+    end
+  end
+  final_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  emoticon_hash = load_library(file_path)
+
+  if emoticon_hash["get_emoticon"][emoticon]
+    return emoticon_hash["get_emoticon"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
+  emoticon_hash = load_library(file_path)
+  if emoticon_hash["get_meaning"][emoticon]
+    return emoticon_hash["get_meaning"][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
